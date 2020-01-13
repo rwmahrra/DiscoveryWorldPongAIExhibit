fix state scaling

ai.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from pong import Pong
 import cv2
 import os
-from utils import encode_action
+from utils import encode_action, discount_rewards
 
 from matplotlib import pyplot as plt
 from vis.visualization import visualize_saliency
@@ -80,21 +80,11 @@ class DQN:
 
     def retrain(self, games):
         states, actions, rewards = games
-        rewards = self.discount_rewards(rewards[:, 1])
+        rewards = discount_rewards(rewards[:, 1], gamma=self.gamma)
         states = np.stack([state.flatten().astype("float32") for state in states], axis=0)
         actions = actions[:, 1]
-
+        print(np.unique(states[0]))
         self.model.fit(x=states, y=actions, sample_weight=rewards, epochs=20)
-
-    def discount_rewards(self, r):
-        """ take 1D float array of rewards and compute discounted reward """
-        discounted_r = np.zeros_like(r, dtype=np.float32)
-        running_add = 0
-        for t in reversed(range(0, r.size)):
-            if r[t] != 0: running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
-            running_add = running_add * self.gamma + r[t]
-            discounted_r[t] = running_add
-        return discounted_r
 
     def save(self, name):
         if not os.path.exists('./models'):

main.py
@@ -15,6 +15,7 @@ from ai import DQN
 from keras import backend as K
 import subprocess
 
+
 def simulate_pong(task_id, show=False):
     try:
         states = np.ndarray(shape=(0, Pong.HEIGHT//4, Pong.WIDTH//4), dtype=np.float32)
@@ -36,7 +37,7 @@ def simulate_pong(task_id, show=False):
             states = np.concatenate((states, np.expand_dims(start_state, axis=0)), axis=0)
             actions = np.concatenate((actions, np.expand_dims(action, axis=0)), axis=0)
             rewards = np.concatenate((rewards, np.asarray([[reward_l, reward_r]], dtype=np.float32)), axis=0)
-            if (show):
+            if show:
                 print(right_action)
                 env.show(2)
                 env.show_state(2)
@@ -73,11 +74,13 @@ def run_simulations(n, threads):
     states = np.ndarray(shape=(0, Pong.HEIGHT // 4, Pong.WIDTH // 4), dtype=np.float32)
     actions = np.ndarray(shape=(0, 2, 2), dtype=np.float32)
     rewards = np.ndarray(shape=(0, 2), dtype=np.float32)
+
     for game in games:
         game_states, game_actions, game_rewards = game
         states = np.concatenate((game_states, states), axis=0)
         actions = np.concatenate((game_actions, actions), axis=0)
         rewards = np.concatenate((game_rewards, rewards), axis=0)
+    print(f"Lasted average {states.shape[0] / n} frames")
     return states, actions, rewards
 
 

visualizer.py
@@ -4,9 +4,10 @@ import time
 import cv2
 import numpy as np
 import player
-from utils import Timer, encode_action
+from utils import Timer, encode_action, discount_rewards
 import multiprocessing
 from ai import DQN
+from main import run_simulations
 import subprocess
 
 
@@ -21,8 +22,23 @@ def view_train_progression(number, neuron):
         dqn.show_weights(neuron)
 
 
-def test_model(id):
+def visualize_game_memory():
+    game = run_simulations(1, 0)
+    game_states, game_actions, game_rewards = game
+    rewards_l = discount_rewards(game_rewards[:, 0])
+    rewards_r = discount_rewards(game_rewards[:, 1])
+    actions_l = game_actions[:, 0]
+    actions_r = game_actions[:, 1]
+    for i in range(len(game_states)):
+        state = game_states[i]
+        reward = rewards_r[i]
+        print(reward)
+        if actions_r[i][0] == 1: print("UP " + str(actions_r[i]))
+        else: print("DOWN " + str(actions_r[i]))
+        cv2.imshow("state", state)
+        cv2.waitKey(0)
 
+def test_model(id):
     dqn = DQN(resume=False)
     dqn.load_model(f"./models/{id}.h5")
     #for i in range(200):
@@ -46,6 +62,23 @@ def test_model(id):
     l, r = env.get_score()
     print(f"Finished game with model {id}, {l} - {r}")
 
+def debug_step():
+    env = Pong()
+    right = player.HumanPlayer('up', 'down')
+    left = player.BotPlayer(env, left=True)
+    last_screen = None
+    state = env.reset()
+    done = False
+    while not done:
+        left_action = left.move(state)
+        right_action = right.move(state)
+        action = np.stack((encode_action(left_action), encode_action(right_action)))
+        start_state = state
+        state, reward, done = env.step(left_action, right_action)
+        reward_l, reward_r = reward
 
-#view_train_progression(12, 1)
-test_model(12)
+        env.show(duration=0)
+        env.show_state(duration=0)
+
+
+test_model(5)