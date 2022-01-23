f1be148a9b1a0c3851fac1da322e3faf94ae2db5
AlexanderNeuwirth <alexander.e.neuwirth@gmail.com>
2022-01-23 01:27:00 -0600

    newtrain_int5_delay2 autojob

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=teaching
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=11
+#SBATCH --output=pong_job.out
+#SBATCH --job-name=newtrain_int5_delay2
+
+# Your job
+#python3 -m pip3 install --user -r requirements.txt
+singularity run --nv /data/containers/msoe-tensorflow-20.07-tf2-py3.sif python3 reinforcement.py
